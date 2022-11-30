261164520532bc651c191987414719d2ba4e67d3
Trayhard11 <83228781+Trayhard11@users.noreply.github.com>
2022-11-30 22:45:30 +0100

    Update combate_pokemon.py

--- combate_pokemon.py
+++ combate_pokemon.py
@@ -18,19 +18,19 @@ while vida_squirtle>0 and vida_pikachu>0:
     print("3.Burbuja que inflinje {} puntos de daño".format(daño_burbuja))
     input_user = input("Que ataque quieres realizar: 1, 2, 3 ")
     if input_user == 1:
-        time.sleep(0.2)
+        time.sleep(1)
         print("Se a realizado el ataque placaje")
         vida_pikachu -= daño_placaje
         print("Ahora la vida de pikachu es de {} puntos de vida".format(vida_pikachu))
 
     elif input_user == 2:
-        time.sleep(0.2)
+        time.sleep(1)
         print("Se arealizado el ataque pistola de agua")
         vida_pikachu -= daño_pistola_agua
         print("Ahora la vida de pikachu es de {} puntos de vida".format(vida_pikachu))
 
     elif input_user == 3:
-        time.sleep(0.2)
+        time.sleep(1)
         print("Se a realizado el ataque burbuja")
         vida_pikachu -= daño_burbuja
         print("Ahora la vida de pikachu es de {} puntos de vida".format(vida_pikachu))
@@ -41,13 +41,13 @@ while vida_squirtle>0 and vida_pikachu>0:
 
     ataque = randint(1,2)
     if ataque == 1:
-        time.sleep(0.2)
+        time.sleep(1)
         print("Pikachu a realizado el ataque de onda trueno")
         vida_squirtle -= daño_onda_trueno
         print("Ahora tu vida es de {} puntos de vida".format(vida_squirtle))
 
     else:
-        time.sleep(0.2)
+        time.sleep(1)
         print("Pikachu a realizado el ataque de bola voltio")
         vida_squirtle -= daño_bola_voltio
         print("Ahora tu vida es de {} puntos de vida".format(vida_squirtle))
